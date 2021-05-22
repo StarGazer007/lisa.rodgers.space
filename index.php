2368270632c6eae1cf752b5dd43975b56b40a308
Lisa Rodgers <lisa@spyvsspycomputers.com>
2021-05-22 08:45:43 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,34 @@
+<html lang="en">
+<head><meta charset="utf-8"><!--Let browser know website is optimized for mobile-->
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<meta name="description" content="">
+<meta name="author" content="WDThemes">
+<meta http-equiv="Cache-control" content="public">
+<link rel="stylesheet" href="https://cdn.lineicons.com/2.0/LineIcons.css">
+<link href="/css/style.css" rel="stylesheet" />
+<title>Lisa Rodgers - Social ID Card</title>
+</head>
+<body>
+
+<div class="wrapper">
+  <div class="news-item hero-item">
+      <H1>Lisa Rodgers</H1>
+  </div>
+  <div class="news-item standard-item">
+      <ul>
+          <li><a href="#"><i class="lni lni-star lni-lg"></i>Thoughts</a></li>
+          <li><a href="#"><i class="lni lni-star lni-lg"></i>Learn</a></li>
+          <li><a href="#"><i class="lni lni-star lni-lg"></i>Recipes/a></li>
+          <li><a href="#"><i class="lni lni-star lni-lg"></i>Buy</a></li>
+          <li><a href="#"><i class="lni lni-star lni-lg"></i>Connect</a></li>
+      </ul>
+  </div>
+  <div class="news-item standard-item">
+<button class="button">Buy Now</button>
+  </div>
+  <div class="news-item standard-item">
+  </div>
+</div>
+
+</body>
+</html>
